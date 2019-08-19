c39adc98ce0cd81be2573e2dbe30d24741bea43e
TaGGoU91 <30494278+TaGGoU91@users.noreply.github.com>
2019-08-19 16:20:30 +0200

    Update vmware.php

--- desktop/php/vmware.php
+++ desktop/php/vmware.php
@@ -5,62 +5,90 @@ if (!isConnect('admin')) {
 $plugin = plugin::byId('vmware');
 sendVarToJS('eqType', $plugin->getId()); // Permet de rendre cliquable les éléments de la page Mes équipements (Mes Serveurs ESXi)
 $eqLogics = eqLogic::byType($plugin->getId()); // Permet de récupérer la liste des équipements de type vmware dans la table eqLogic
-
-
-// pour le débug -> permet d'afficher sur la console du navigateur en appelant la fonction console_log
-function console_log($output, $with_script_tags = true) {
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
-    }
-    echo $js_code;
-}
-
 ?>
 
 <div class="row row-overflow">
- <div class="col-lg-12 eqLogicThumbnailDisplay">
-	<legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
-	<div class="eqLogicThumbnailContainer">
+    <!--<div class="col-xs-12 eqLogicThumbnailDisplay"> --> <!-- TOUT ça c'était pour le bandeau à gauche qui apparaissait au survol -->
+        <!--<div class="bs-sidebar">
+            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un équipement}}</a>
+                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+                <?php/*
+foreach ($eqLogics as $eqLogic) {
+	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '" style="' . $opacity .'"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+}
+*/		    ?>
+           </ul>
+       </div>
+   </div>-->
+
+   <!--<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">-->
+   <div class="col-xs-12 eqLogicThumbnailDisplay">
+   <!-- <legend>{{Mes équipements}}</legend>-->
+  <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
+  <div class="eqLogicThumbnailContainer">
+     <!-- <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+        <i class="fa fa-plus-circle" style="font-size : 6em;color:#00A9EC;"></i>
+        <br>
+        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#00A9EC">{{Ajouter}}</span>
+    </div> Remplacer par le bloc en dessous -->
 	    <div class="cursor eqLogicAction logoPrimary" data-action="add">
 			<i class="fas fa-plus-circle"></i>
 			<br>
 			<span>{{Ajouter}}</span>
 		</div>
+  <!--<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+      <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+    <br>
+    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
+  </div> remplacer par le bloc en dessous -->
 		<div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
 			<i class="fas fa-wrench"></i>
 			<br>
 			<span>{{Configuration}}</span>
 		</div>
+  <!--<div class="cursor" id="bt_healthvmware" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+        <center>
+          <i class="fa fa-medkit" style="font-size : 6em;color:#767676;"></i>
+        </center>
+        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Santé}}</center></span>
+      </div> remplacer par le bloc en dessous -->
 		<div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf" id="bt_healthvmware"> <!-- l'action est traitée dans le vmware.js -->
-			<i class="fas fa-medkit"></i>
+			<i class="fas fa-wrench"></i>
 			<br>
-			<span>{{Santé}}</span>
+			<span>{{Configuration}}</span>
 		</div>
 	</div>
 	<legend><i class="fas fa-table"></i> {{Mes serveurs ESXi}}</legend>
-		<input class="form-control" placeholder="{{Mes équipements VMWARE}}" id="in_searchEqlogic" />
+		<input class="form-control" placeholder="{{Mes ESXi(s) ou VM(s) connus}}" id="in_searchEqlogic" />
 	<div class="eqLogicThumbnailContainer">
     <?php
 		foreach ($eqLogics as $eqLogicEsxiHost) {
 			if ($eqLogicEsxiHost->getConfiguration('type') == 'ESXi') {
             	echo '<legend>' . $eqLogicEsxiHost->getHumanName(true) . '</legend>';
 				echo '<div class="eqLogicThumbnailContainer">';
+				// $opacity = ($eqLogicEsxiHost->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogicEsxiHost:style:noactive');
 				$opacity = ($eqLogicEsxiHost->getIsEnable()) ? '' : 'disableCard';
+				// echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogicEsxiHost->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 				echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogicEsxiHost->getId() . '">';
-				// On affiche une image différente pour le serveur ESXi pour le répérer plus facilement
-					echo '<img src="plugins/vmware/docs/assets/images/icone_esxi.jpg" height="105" width="95">';
-					echo '<br>';
-					echo '<span class="name">' . $eqLogicEsxiHost->getHumanName(true, true) . '</span>';
+				// On affiche une image différente pour le serveur eSXi pour le répérer plus facilement
+				echo '<img src="plugins/vmware/docs/assets/images/icone_esxi.jpg" height="105" width="95">';
+				echo '<br>';
+				// echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogicEsxiHost->getHumanName(true, true) . '</span>';
+				echo '<span class="name">' . $eqLogicEsxiHost->getHumanName(true, true) . '</span>';
 				echo '</div>';
 				foreach ($eqLogics as $eqLogicVM) {
 					if ($eqLogicVM->getConfiguration('type') == 'vm' && $eqLogicVM->getConfiguration('ESXiHostIpAddress') == $eqLogicEsxiHost->getConfiguration('ipAddress')) {
+						// $opacity = ($eqLogicVM->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogicVM:style:noactive');
 						$opacity = ($eqLogicVM->getIsEnable()) ? '' : 'disableCard';
+						// echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogicVM->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 						echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogicVM->getId() . '">';
-							echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
-							echo "<br>";
-							echo '<span class="name">' . $eqLogicVM->getHumanName(true, true) . '</span>';
+						// echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
+						echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+						echo "<br>";
+						// echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogicVM->getHumanName(true, true) . '</span>';
+						echo '<span class="name">' . $eqLogicVM->getHumanName(true, true) . '</span>';
 						echo '</div>';
 					}
 				}
@@ -69,7 +97,16 @@ function console_log($output, $with_script_tags = true) {
 		}
 	?>	
 	</div>
-  </div>
+
+<!--<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+	<a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+  </ul> remplacer par en dessous -->
   <div class="col-xs-12 eqLogic" style="display: none;">
 		<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
@@ -81,7 +118,7 @@ function console_log($output, $with_script_tags = true) {
     <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
     <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
   </ul>
-   <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+  <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
     <div role="tabpanel" class="tab-pane active" id="eqlogictab">
       <br/>
 		<div class="row">
@@ -181,20 +218,22 @@ function console_log($output, $with_script_tags = true) {
 		</div>
 	</div>
       <div role="tabpanel" class="tab-pane" id="commandtab">
-		<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
-			<table id="table_cmd" class="table table-bordered table-condensed">
-				<thead>
-					<tr>
-						<th>{{Nom}}</th><th>{{Type}}</th><th>{{Configuration}}</th><th>{{Action}}</th>
-					</tr>
-				</thead>
-				<tbody>
-				</tbody>
-			</table>
-	  </div>
-   </div>
- </div>
+<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
+<table id="table_cmd" class="table table-bordered table-condensed">
+    <thead>
+        <tr>
+            <th>{{Nom}}</th><th>{{Type}}</th><th>{{Configuration}}</th><th>{{Action}}</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+</div>
+</div>
+
+</div>
 </div>
 
 <?php include_file('desktop', 'vmware', 'js', 'vmware');?>
 <?php include_file('core', 'plugin.template', 'js');?>
+
