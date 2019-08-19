c9fc150c4567c89d0cde09f25f773e049159ed54
TaGGoU91 <30494278+TaGGoU91@users.noreply.github.com>
2019-08-19 16:22:12 +0200

    Update vmware.php

--- desktop/php/vmware.php
+++ desktop/php/vmware.php
@@ -8,20 +8,20 @@ $eqLogics = eqLogic::byType($plugin->getId()); // Permet de récupérer la liste
 ?>
 
 <div class="row row-overflow">
-    <!--<div class="col-xs-12 eqLogicThumbnailDisplay"> --> <!-- TOUT ça c'était pour le bandeau à gauche qui apparaissait au survol -->
-        <!--<div class="bs-sidebar">
+    <div class="col-lg-2 col-md-3 col-sm-4">
+        <div class="bs-sidebar">
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
                 <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un équipement}}</a>
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-                <?php/*
+                <?php
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '" style="' . $opacity .'"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
 }
-*/		    ?>
+		    ?>
            </ul>
        </div>
-   </div>-->
+   </div>
 
    <!--<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">-->
    <div class="col-xs-12 eqLogicThumbnailDisplay">
