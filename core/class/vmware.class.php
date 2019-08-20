def0367ed9873317b11a4b66fdf7233a285016bb
TaGGoU91 <30494278+TaGGoU91@users.noreply.github.com>
2019-08-20 14:34:24 +0200

    Update vmware.class.php

--- core/class/vmware.class.php
+++ core/class/vmware.class.php
@@ -674,7 +674,8 @@ class vmware extends eqLogic {
 		  $os = $vm['GuestId'];
 		  //$started = str_replace(array("notRunning","running"), array("No","Yes"), $vm['PowerState'] );
 		  $started = str_replace(array("Powered off","Powered on"), array("Non","Oui"), $vm['PowerState'] );
-		  $toolsStatus = str_replace(array("toolsNotInstalled","toolsOK","toolsNotRunning"), array("Non Installé","Oui","Non Démarré"), $vm['vmwareTools'] );
+		  $toolsStatus = str_replace(array("toolsNotInstalled","toolsOk","toolsNotRunning"), array("Non Installé","Oui","Non Démarré"), $vm['vmwareTools'] );
+		 // log::add('vmware', 'debug', 'valeur dans saveVmAsEquipement de toolsStatus ' . $toolsStatus );
 		 
 		  $vmware = self::byLogicalId('vmware'.$deviceid,'vmware'); // Création de l'enveloppe vide d'une VM
 		  if (!is_object($vmware)) {
