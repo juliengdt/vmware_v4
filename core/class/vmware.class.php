2875c672ac34345b11a8c961a8fb26995dffa11f
TaGGoU91 <30494278+TaGGoU91@users.noreply.github.com>
2019-08-21 21:50:02 +0200

    Update vmware.class.php

--- core/class/vmware.class.php
+++ core/class/vmware.class.php
@@ -544,14 +544,15 @@ class vmware extends eqLogic {
 				log::add('vmware', 'debug', 'Func refreshViaBouttonSynchroniser IF car on a trouvé l\'esxi par son ID dont voici le nom : '.$eqLogicEsxiHost->getConfiguration("name"));
 				if ($eqLogicEsxiHost->getIsEnable() == 1) { //Vérifie que l'équipement est actif
 					log::add('vmware', 'debug', 'DEBUT DU IF ENABLE');
-					$cmd = $eqLogicEsxiHost->getCmd(null, 'refresh'); // stocke la commande refresh, si elle existe
-					log::add('vmware', 'debug', 'JUSTE APRES LA RECHERCHE DE LA COMMANDE REFRESH');
-					if (!is_object($cmd)) { // si la commande n'existe pas on continue à la chercher via le foreach
-						log::add('vmware', 'debug', 'DANS LE IF de la recherche de la commande, donc on l\'a trouvée');
-						 continue; 
-					}else {
-						 log::add('vmware', 'debug', 'DANS LE ELSE de la recherche de la commande, ON LE PAS TROUVEE');
-					}				 
+					$eqLogicEsxiHost->save();
+					//$cmd = $eqLogicEsxiHost->getCmd(null, 'refresh'); // stocke la commande refresh, si elle existe
+					//log::add('vmware', 'debug', 'JUSTE APRES LA RECHERCHE DE LA COMMANDE REFRESH');
+					//if (!is_object($cmd)) { // si la commande n'existe pas on continue à la chercher via le foreach
+					//	log::add('vmware', 'debug', 'DANS LE IF de la recherche de la commande, donc on l\'a trouvée');
+					//	 continue; 
+					//}else {
+					//	 log::add('vmware', 'debug', 'DANS LE ELSE de la recherche de la commande, ON NE LA TROUVE PAS');
+					//}				 
 					log::add('vmware', 'info', 'début du refresh via la fonction refreshViaBouttonSynchroniser');
 					// $cmd->execCmd(); // on a trouvé la commande, on l'exécute (Pas besoin d'une boucle else ? se renseigner sur la commande continue, semble permettre de sortir de la boucle;
 					log::add('vmware', 'info', 'Fin du refresh via la fonction refreshViaBouttonSynchroniser');
