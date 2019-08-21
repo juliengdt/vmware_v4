b0afb92c2b009b16d4454eb42ad92badd2ab8a5c
TaGGoU91 <30494278+TaGGoU91@users.noreply.github.com>
2019-08-21 21:24:50 +0200

    Update vmware.class.php

--- core/class/vmware.class.php
+++ core/class/vmware.class.php
@@ -533,9 +533,9 @@ class vmware extends eqLogic {
 		log::add('vmware', 'info', '========================================================');
 		log::add('vmware', 'info', '===== Début du log - refreshViaBouttonSynchroniser =====');
 		log::add('vmware', 'info', '========================================================');
-		log::add('vmware', 'info', 'valeur de l\'ID'. $idToSearch .'');
+		log::add('vmware', 'info', 'valeur de l\'ID : '. $idToSearch .'');
 		 log::add('vmware', 'info', 'Juste avant la récupération de l\'eqLogic');
-	//	 $eqLogic = vmware::byId($idToSearch));
+		 $eqLogic = eqLogic::byId($idToSearch));
 		 log::add('vmware', 'info', 'Juste après la récupération de l\'eqLogic'. $eqLogic.'');
 		 
 		 //if ($this->getIsEnable() == 1) { //Vérifie que l'équipement est actif
