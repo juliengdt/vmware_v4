eebbc7cd09949af46fc65077c27a079b4f46fdc5
TaGGoU91 <30494278+TaGGoU91@users.noreply.github.com>
2019-08-21 23:37:44 +0200

    Update vmware.php

--- desktop/php/vmware.php
+++ desktop/php/vmware.php
@@ -46,8 +46,7 @@ function console_log($output, $with_script_tags = true) {
 			if ($eqLogicEsxiHost->getConfiguration('type') == 'ESXi') {
             	console_log('ESXI trouvé ' . $eqLogicEsxiHost->getConfiguration('name') . '');
 				echo '<legend>' . $eqLogicEsxiHost->getHumanName(true) . '</legend>';
-				echo '<div class="eqLogicThumbnailContainer">'; // Permet d'avoir le deuxième ESXi aligné à gauche, mais il se retrouve bien trop bas ( voir si c'est la présence de VM qui pose problème
-				//echo '<div class="cursor eqLogicAction" id="bt_synchroEqLogic" data-id="' . $eqLogicEsxiHost->getId() . '">'; // l'action est traitée dans le vmware.js le data-id permet de récupérer l'info dans le JS pour transmettre l'appel à la fonction refresh pour l'ESXi en question uniquement
+				echo '<div class="eqLogicThumbnailContainer">';
 				echo '<div class="cursor eqLogicAction synchronisation"  data-id="' . $eqLogicEsxiHost->getId() . '">'; // l'action est traitée dans le vmware.js le data-id permet de récupérer l'info dans le JS pour transmettre l'appel à la fonction refresh pour l'ESXi en question uniquement // on se base sur le terme synchronisation pour le retrouver dans le JS
 				echo '<i class="fas fa-sync"></i>';
 				echo '<br>';
