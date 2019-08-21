0721a4446e23aa2411ec62a0c97f7966857dadc5
TaGGoU91 <30494278+TaGGoU91@users.noreply.github.com>
2019-08-21 14:44:25 +0200

    Update vmware.class.php

--- core/class/vmware.class.php
+++ core/class/vmware.class.php
@@ -162,14 +162,7 @@ class vmware extends eqLogic {
 				}
 			}
 				log::add('vmware', 'debug', 'Valeur de TO BE UPDATED : '.$toBeUpdated .'');
-				//$eqLogicEsxiHost->checkAndUpdateCmd('toBeUpdated', $toBeUpdated); 
-				log::add('vmware', 'debug', 'avant la mise à jour du nombre de vm ');
-				$eqLogicEsxiHost->checkAndUpdateCmd('nbVM', '1');
-				log::add('vmware', 'debug', 'après la mise à jour du nombre de vm ');
-				
-				log::add('vmware', 'debug', 'avant la mise à jour du statut ToBeUpdated');
-				$eqLogicEsxiHost->checkAndUpdateCmd('toBeUpdated', 'Yes'); 
-				log::add('vmware', 'debug', 'avant la mise à jour du statut ToBeUpdated');
+				$eqLogicEsxiHost->checkAndUpdateCmd('toBeUpdated', $toBeUpdated); 
 				$closesession = ssh2_exec($connection, 'exit'); // Fermeture de la connexion SSH à l'hyperviseur
 				stream_set_blocking($closesession, true);
 				stream_get_contents($closesession);		
