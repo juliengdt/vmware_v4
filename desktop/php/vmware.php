fc88987a25d951c2fff4068d1f0dffdb0f65cf9a
TaGGoU91 <30494278+TaGGoU91@users.noreply.github.com>
2019-08-23 14:52:48 +0200

    Update vmware.php

--- desktop/php/vmware.php
+++ desktop/php/vmware.php
@@ -50,8 +50,8 @@ function console_log($output, $with_script_tags = true) {
 				echo '<div class="eqLogicThumbnailContainer">'; 
 				//eqLogicAction
 				echo '<div class="eqLogicAction cursor synchronisation"  data-id="' . $eqLogicEsxiHost->getId() . '">'; // l'action est traitée dans le vmware.js le data-id permet de récupérer l'info dans le JS pour transmettre l'appel à la fonction refresh pour l'ESXi en question uniquement // on se base sur le terme synchronisation pour le retrouver dans le JS
-				echo '<img src="plugins/vmware/docs/assets/images/icone_esxi.png">';
-				//echo '<i class="fas fa-sync"></i>';
+			//	echo '<img src="plugins/vmware/docs/assets/images/icone_esxi.png">';
+				//echo '<img><i class="fas fa-sync"></i></img>';
 				echo '<br>';
 				echo '<span class="name">{{Synchroniser}}</span>'; // rajout du class="name" pour tester d'aligner le bouton synchroniser correctement
 				echo '</div>';
